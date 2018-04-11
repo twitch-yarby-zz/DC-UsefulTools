fbd0d3bd1599156f3f5aab6339bf7fcdd29b9f6e
Twitch Yarby <twitch.yarby@gmail.com>
2018-04-10 20:33:32 -0500

    Revert Write-Output change for Rename-VMList

--- PowerCLI scripts/Rename-VMList.ps1
+++ PowerCLI scripts/Rename-VMList.ps1
@@ -8,10 +8,10 @@ Import-Module -Name VMware.VimAutomation.Core -ErrorAction Stop
 
 Set-PowerCLIConfiguration -InvalidCertificationAction ignore -confirm:$false
 
-Write-Output "Reading in file"
+Write-Host "Reading in file"
 $renameList = Import-CSV $vmList
 
-Write-Output "Connecting to vCenter"
+Write-Host "Connecting to vCenter"
 $session = Connect-ViServer -Server $vcIP -User $vcUser -Password $vcPass
 
 if($session.IsConnected -ne $true)
@@ -20,17 +20,17 @@ if($session.IsConnected -ne $true)
 	exit 1
 }
 
-Write-Output "Begining rename"
+Write-Host "Begining rename"
 ForEach($vmName in $renameList)
 {
 	$VM = Get-VM -Name $vmName.OLD_NAME
 	if($VM)
 	{
-		Write-Output "Found VM: " $VM.Name
+		Write-Host "Found VM: " $VM.Name
 		$VM = Set-VM $VM -Name $vmName.NEW_NAME -Confirm:$false
 		if($VM.Name -eq $vmName.NEW_NAME)
 		{
-			Write-Output $vmName.NEW_NAME " success" -ForegroundColor Green
+			Write-Host $vmName.NEW_NAME " success"
 		}
 		else
 		{
@@ -42,7 +42,7 @@ ForEach($vmName in $renameList)
 		$VM = Get-VM -Name $vmName.NEW_NAME
 		if ($VM)
 		{
-			Write-Output $vmName.NEW_NAME " already renamed" -ForegroundColor Green
+			Write-Host $vmName.NEW_NAME " already renamed" -ForegroundColor Green
 		}
 		else
 		{
@@ -51,5 +51,5 @@ ForEach($vmName in $renameList)
 	}
 }
 
-Write-Output "Operation complete. Disconnecting"
+Write-Host "Operation complete. Disconnecting"
 Disconnect-ViServer $session -Confirm:$false
